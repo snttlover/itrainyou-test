61b4e1804bb29e07cfdfd9a3c2ba8b52c5424802
Ivan <ivan.sherstnyuk@gmail.com>
2020-02-26 22:22:58 +0000

    Revert "[Improve] Remove unused types"
    This reverts commit 26de7d2782eacc8b6ed452d63a3de14ba3788526.

--- src/application/AppStyles.tsx
+++ src/application/AppStyles.tsx
@@ -1,34 +1,13 @@
 import { createGlobalStyle } from "styled-components"
 
-const fontFace = (
-  name: string,
-  src: string,
-  fontWeight: string | number = "normal",
-  fontStyle = "normal"
-) => {
-  return `
-      @font-face{
-          font-family: "${name}";
-          src: url(${src}.woff) format("woff"),
-               url(${src}.woff2) format("woff2"),
-               url(${src}.ttf) format("truetype");
-
-          font-style: ${fontStyle};
-          font-weight: ${fontWeight};
-      }
-  `
-}
-
 export const AppStyles = createGlobalStyle`
-  ${fontFace("Gilroy", "/fonts/gilroy/Gilroylight", 300, "normal")}
-  ${fontFace("Gilroy", "/fonts/gilroy/Gilroyextrabold", 800, "normal")}
-
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: Gilroy;
+    font-family: Gilroy, sans-serif;
   }
+  
   html,
   body {
     width: 100%;
