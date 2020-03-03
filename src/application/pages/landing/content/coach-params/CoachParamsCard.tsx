9ea7eb0910c1a920137e760adb2fb77955c2e692
vlkf27 <vlkf27@users.noreply.github.com>
2020-03-03 23:33:50 +0300

    fix: Адапт 768. Изменение шрифта описания типов коучинга в блоке типов коучинга с 16 на 14

--- src/application/pages/landing/content/coach-params/CoachParamsCard.tsx
+++ src/application/pages/landing/content/coach-params/CoachParamsCard.tsx
@@ -57,6 +57,10 @@ const Description = styled.div`
   font-size: 16px;
   line-height: 22px;
   color: #424242;
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+    line-height: 20px;
+  }
 `
 
 type CoachParamsCardProps = {
