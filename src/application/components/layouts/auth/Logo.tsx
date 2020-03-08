a60c6ab52ae09717233965b8e95cb8b8c12d5cff
vlkf27 <vlkf27@users.noreply.github.com>
2020-03-08 05:37:50 +0300

    fix: Заменил лого лейаута авторизации на картинку

--- src/application/components/layouts/auth/Logo.tsx
+++ src/application/components/layouts/auth/Logo.tsx
@@ -1,48 +1,21 @@
-import * as React from "react"
 import styled from "styled-components"
-import { Logo as LogoImage } from "@/application/pages/landing/content/top-bar/logo/Logo"
+import logo from "./images/logo.svg"
 
-const LogoContainer = styled.div`
+export const Logo = styled.img.attrs({ src: logo })`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 71px;
   text-align: center;
   position: absolute;
   left: 22px;
   top: 25px;
+  width: 80px;
+  height: 80.26px;
   @media screen and (max-width: 480px) {
     left: 16px;
     top: 16px;
+    width: 60px;
+    height: 60.19px;
   }
 `
-
-const Text = styled.div`
-  text-transform: uppercase;
-  width: 100%;
-  font-weight: bold;
-  margin-top: 12px;
-  color: #544274;
-  font-size: 20px;
-  line-height: 26px;
-  @media screen and (max-width: 480px) {
-    font-size: 16px;
-  }
-`
-
-const StyledLogo = styled(LogoImage)`
-  width: 35px;
-  height: 35px;
-  @media screen and (max-width: 480px) {
-    width: 26px;
-    height: 26px;
-  }
-`
-
-export const Logo = () => (
-  <LogoContainer>
-    <StyledLogo />
-    <Text>i train you</Text>
-  </LogoContainer>
-)
