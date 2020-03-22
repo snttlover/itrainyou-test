e046e717294aa77f3313d863df14980bf073e546
vlkf27 <vlkf27@users.noreply.github.com>
2020-03-22 14:38:36 +0300

    feat: Валидация изменение пароля

---
+++ src/application/pages/auth/pages/reset-password/reset-password.model.ts
@@ -0,0 +1,85 @@
+import { ResetPasswordRequest, resetPassword } from "@/application/lib/api/reset-password"
+import { appDomain } from "@/application/store"
+
+import { createEffectorField } from "@app/lib/generators/efffector"
+import { passwordValidator, trimString } from "@app/lib/validators"
+import { AxiosError } from "axios"
+import { combine, createStoreObject } from "effector"
+import { signUpDomain } from "@app/pages/auth/pages/signup/signup.model"
+import { navigate } from "@reach/router"
+
+const resetDomain = appDomain.createDomain()
+
+export const resetFormSended = resetDomain.createEvent()
+
+type ResetRType = {
+  token: string
+  password: string
+}
+
+export const resetFx = resetDomain.createEffect<ResetRType, ResetPasswordRequest, AxiosError>({
+  handler: ({ password, token }) => resetPassword({ password, token })
+})
+
+resetFx.done.watch(data => {
+  console.log(data.result.token)
+  // @ts-ignore
+  window.location = `/`
+})
+
+export const [$password, passwordChanged, $passwordError, $isPasswordCorrect] = createEffectorField<string>({
+  domain: resetDomain,
+  defaultValue: "",
+  validator: passwordValidator,
+  eventMapper: event => event.map(trimString)
+})
+
+export const [
+  $passwordRepeat,
+  passwordRepeatChanged,
+  $passwordRepeatError,
+  $isPasswordRepeatCorrect
+] = createEffectorField<string>({
+  domain: signUpDomain,
+  defaultValue: "",
+  validator: value => {
+    const error = passwordValidator(value)
+    const passwordValue = $password.getState()
+    if (passwordValue !== value) return "Пароли не совпадают"
+    return error
+  },
+  eventMapper: event => event.map(trimString)
+})
+
+export const $commonError = resetDomain
+  .createStore<string | null>(null)
+  .on(resetFx, () => null)
+  .on(resetFx.fail, (state, { error }) => {
+    if (error.response?.data.token) {
+      return `Ссылка больше недействительна`
+    }
+
+    return `Произошла ошибка при изменении пароля`
+  })
+
+export const $resetForm = createStoreObject({
+  password: $password,
+  passwordRepeat: $passwordRepeat
+})
+
+export const $resetFormErrors = createStoreObject({
+  password: $passwordError,
+  passwordRepeat: $passwordRepeatError
+})
+
+export const $isFormValid = combine(
+  $isPasswordCorrect,
+  $isPasswordRepeatCorrect,
+  (isPasswordCorrect, isPasswordRepeatCorrect) => isPasswordCorrect && isPasswordRepeatCorrect
+)
+
+export const updateResetSuccessMessageVisibility = resetDomain.createEvent<boolean>()
+export const $resetSuccessMessageVisibility = resetDomain
+  .createStore(false)
+  .on(updateResetSuccessMessageVisibility, (state, status) => status)
+  .on(resetFx.done, () => true)
