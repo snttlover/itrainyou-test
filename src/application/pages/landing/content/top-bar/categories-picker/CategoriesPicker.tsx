b47a54639c46be14df6fcdf60b4471099e5e8f17
vlkf27 <vlkf27@users.noreply.github.com>
2020-04-02 02:29:28 +0300

    feat: Описание коучей

--- src/application/pages/landing/content/top-bar/categories-picker/CategoriesPicker.tsx
+++ src/application/pages/landing/content/top-bar/categories-picker/CategoriesPicker.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import * as React from "react"
 import styled from "styled-components"
 import { Dropdown } from "@/application/components/dropdown/Dropdown"
@@ -49,6 +49,54 @@ const StyledDropdown = styled(Dropdown)`
   width: 156px;
 `
 
+type CheckBoxWrapperTypes = {
+  children: React.ReactNode,
+  description: string
+}
+
+const Description = styled.div`
+  position: fixed;
+  z-index: 200;
+  background: #FFFFFF;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.14), 0px 2px 2px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2);
+  font-size: 16px;
+  line-height: 22px;
+  padding: 16px;
+  width: 400px;
+  border-radius: 4px;
+`
+
+const StyledCheckboxWrapper = styled.div``
+
+const CheckBoxWrapper = (props: CheckBoxWrapperTypes) => {
+  const [descriptionVisibility, changeDescriptionVisibility] = useState(false)
+  const [coords, changeCoords] = useState({ x: 0, y: 0 })
+
+  const mouseMoveHandler = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    changeDescriptionVisibility(true)
+    changeCoords({
+      x: event.clientX + 20,
+      y: event.clientY + 20
+    })
+  }
+
+  const mouseLeaveHandler = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    changeDescriptionVisibility(false)
+  }
+
+  const descriptionStyles = {
+    left: `${coords.x}px`,
+    top: `${coords.y}px`
+  }
+
+  return (
+    <StyledCheckboxWrapper onMouseMove={mouseMoveHandler} onMouseLeave={mouseLeaveHandler}>
+      {descriptionVisibility && <Description style={descriptionStyles}>{props.description}</Description>}
+      {props.children}
+    </StyledCheckboxWrapper>
+  )
+}
+
 type CategoriesPickerTypes = {
   className?: string
 }
@@ -65,10 +113,12 @@ export const CategoriesPicker = (props: CategoriesPickerTypes) => {
 
   const renderCheckboxes = () =>
     useList($categoriesList, item => (
-      <StyledCheckbox value={item.checked} onChange={e => toggleCategorySelection(item.id)}>
-        <CategoryIcon src={item.icon} />
-        <Text>{item.name}</Text>
-      </StyledCheckbox>
+      <CheckBoxWrapper description={item.description}>
+        <StyledCheckbox value={item.checked} onChange={e => toggleCategorySelection(item.id)}>
+          <CategoryIcon src={item.icon} />
+          <Text>{item.name}</Text>
+        </StyledCheckbox>
+      </CheckBoxWrapper>
     ))
 
   return (
