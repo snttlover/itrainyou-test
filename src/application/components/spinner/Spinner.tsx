b117f9a1db5a7d506b661c71570573a114488430
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-04-01 17:45:38 +0300

    Исправил центрирование спинера

--- src/application/components/spinner/Spinner.tsx
+++ src/application/components/spinner/Spinner.tsx
@@ -4,6 +4,8 @@ import spinner from "./spinner.svg"
 
 const Wrapper = styled.div`
   position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background: rgba(255, 255, 255, 0.8);
