4dd82d6ade21170eaa58fc42ac68a58a36c15f7b
vlkf27 <vlkf27@users.noreply.github.com>
2020-05-15 09:32:32 +0300

    fix: некоторые коучи спрятались

--- src/application/pages/dashboard-layout/DashboardLayout.tsx
+++ src/application/pages/dashboard-layout/DashboardLayout.tsx
@@ -1,3 +1,3 @@
 import {DashboardLayout} from "@/application/components/layouts/dashboard/DashboardLayout"
 
-export const DashboardLayoutPage = () => <DashboardLayout backgroundColor={`#000`} >helo</DashboardLayout>
+export const DashboardLayoutPage = () => <DashboardLayout>helo</DashboardLayout>
