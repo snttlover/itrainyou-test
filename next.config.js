57e7d513523cfbd3ce0f3d88a826f6bc0f405963
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-05-16 16:17:22 +0300

    Stop optimize svg

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
-const path = require('path')
-const withImages = require('next-optimized-images')
-const { withEffectoReactAliases } = require("effector-next/tools");
+const path = require("path")
+const withImages = require("next-optimized-images")
+const { withEffectoReactAliases } = require("effector-next/tools")
 
-const enhance = withEffectoReactAliases();
+const enhance = withEffectoReactAliases()
 
-module.exports = enhance(withImages({
-  env: {
-    BACKEND_URL: ''
-  },
-  webpack(config, options) {
-    config.resolve.alias['@'] = path.join(__dirname, 'src')
-    config.resolve.alias['@app'] = path.join(__dirname, 'src')
-    return config
-  }
-}))
+module.exports = enhance(
+  withImages({
+    optimizeImagesInDev: true,
+    env: {
+      BACKEND_URL: ""
+    },
+    webpack(config, options) {
+      config.resolve.alias["@"] = path.join(__dirname, "src")
+      config.resolve.alias["@app"] = path.join(__dirname, "src")
+      return config
+    }
+  })
+)
