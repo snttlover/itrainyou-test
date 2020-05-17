ee02d114b4094ddbc08133523839cb3d5357a87e
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-05-17 17:50:14 +0300

    Rework Coach header

--- src/application/lib/responsive/media.ts
+++ src/application/lib/responsive/media.ts
@@ -1,8 +1,8 @@
 import { generateMedia } from "styled-media-query"
 
 export const MediaRange = generateMedia({
-  mobile: '480px',
-  tablet: '769px',
-  laptop: '1024px',
-  desktop: '1280px'
+  mobile: "480px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "1280px"
 })
