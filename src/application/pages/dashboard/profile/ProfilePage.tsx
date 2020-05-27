aee6bc12650396f911ce1c266cc21cfde7dc9cf7
vlkf27 <vlkf27@users.noreply.github.com>
2020-05-27 10:05:13 +0300

    wip: init profile page

---
+++ src/application/pages/dashboard/profile/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import {DashboardLayout} from "@/application/components/layouts/behaviors/dashboard/DashboardLayout"
+
+
+export default () => (
+  <DashboardLayout>
+    profile
+  </DashboardLayout>
+)
