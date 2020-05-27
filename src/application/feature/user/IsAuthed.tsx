628724b8febe5f4041833e8e89d29ce1ed9f6ab8
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-05-27 21:50:35 +0300

    Create IsAuthed and IsGuest components

---
+++ src/application/feature/user/IsAuthed.tsx
@@ -0,0 +1,11 @@
+import { $isLoggedIn } from "@/application/feature/user/user.model"
+import { useStore } from "effector-react/ssr"
+import React from "react"
+
+type IsAuthedProps = {
+  children: React.ReactChild
+}
+
+export const IsAuthed = ({ children }: IsAuthedProps) => {
+  return useStore($isLoggedIn) ? children : null
+}
