1068d9e87e1a4e0bf33f136bf40b12eaf016709d
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-06-02 01:53:26 +0300

    home

--- src/application/pages/home/HomePage.tsx
+++ src/application/pages/home/HomePage.tsx
@@ -1,10 +1,46 @@
+import { CoachCard } from "@/application/components/coach-card/CoachCard"
 import { UserLayout } from "@/application/components/layouts/behaviors/user/UserLayout"
-import React from "react"
+import { useStore } from "effector-react"
 
-export const HomePage = () => (
-  <UserLayout>
-    <div></div>
-  </UserLayout>
-)
+import { $recommendations, mounted } from "./home.model"
+import React, { useEffect } from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  padding: 16px;
+`
+
+const Title = styled.div`
+  font-family: Roboto Slab;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 26px;
+  color: #424242;
+`
+
+const RecommendationCoachCard = styled(CoachCard)`
+  margin-top: 12px;
+`
+
+export const HomePage = () => {
+  const recommendations = useStore($recommendations)
+  useEffect(() => {
+    mounted()
+  }, [])
+
+  return (
+    <UserLayout>
+      <Container>
+        <Title>Сессия уже началась!</Title>
+        <Title>У вас сегодня</Title>
+        <Title>Рекомендации</Title>
+        {recommendations.map(coach => (
+          <RecommendationCoachCard coach={coach} />
+        ))}
+      </Container>
+    </UserLayout>
+  )
+}
 
 export default HomePage
