d648a03b63b99c58a250b1a4c3a9c6d97a25655c
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-06-09 22:26:53 +0300

    remove examples

--- src/application/feature/coach-placeholder/examples/WithClockExample.tsx
+++
@@ -1,7 +0,0 @@
-import { CoachSectionPlaceholder } from "@/application/feature/coach-placeholder/CoachSectionPlaceholder"
-import { GirlWithClock } from "@/application/feature/coach-placeholder/right-images/GirlWithClock"
-import React from "react"
-
-const features = [`Смотреть список своих клиентов`, `Искать по своим клиентам`]
-
-export const WithClockExample = () => <CoachSectionPlaceholder features={features} renderImage={() => <GirlWithClock />} />
--- src/application/feature/coach-placeholder/examples/WithPhoneExample.tsx
+++
@@ -1,9 +0,0 @@
-import { CoachSectionPlaceholder } from "@/application/feature/coach-placeholder/CoachSectionPlaceholder"
-import { PhoneWithGirl } from "@/application/feature/coach-placeholder/right-images/PhoneWithGirl"
-import React from "react"
-
-const features = [`Смотреть список своих клиентов`, `Искать по своим клиентам`, `Переходить к диалогу с клиентом`]
-
-export const WithPhoneExample = () => (
-  <CoachSectionPlaceholder features={features} renderImage={() => <PhoneWithGirl />} />
-)
