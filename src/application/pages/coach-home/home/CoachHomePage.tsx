4fdda8449e1c930ca2633da3304e1aaf15b55518
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-06-09 14:52:18 +0300

    Move Coach home page

--- src/application/pages/coach-home/home/CoachHomePage.tsx
+++ src/application/pages/coach-home/home/CoachHomePage.tsx
@@ -1,1 +1,23 @@
-export const CoachHomePage = () => <div>HomePage</div>
+import styled from "styled-components"
+import { StartedSessions } from "@/application/pages/coach-home/home/content/StartedSessions"
+import { MediaRange } from "@/application/lib/responsive/media"
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 672px;
+  margin-top: 36px;
+  margin-left: 40px;
+  padding: 0 16px;
+  ${MediaRange.lessThan(`tablet`)`
+    margin: 0 auto;
+    margin-top: 40px;
+  `}
+`
+
+export const CoachHomePage = () => (
+  <Container>
+    <StartedSessions />
+    <StartedSessions />
+    <StartedSessions />
+  </Container>
+)
--- src/pages/coach/sessions-page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic"
-import React from "react"
-
-const SessionsPage = dynamic(() => import("@/application/pages/coach/pages/sessions-page/SessionsPage"), {
-  ssr: false,
-})
-
-export default SessionsPage
