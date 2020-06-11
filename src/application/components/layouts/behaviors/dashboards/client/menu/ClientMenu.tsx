9e0aa72345064d5926719e03cc7164a37f8e46e7
vlkf27 <vlkf27@users.noreply.github.com>
2020-06-11 05:23:38 +0300

    feat: Прокинул аватары и имена пользователей в дешборды

--- src/application/components/layouts/behaviors/dashboards/client/menu/ClientMenu.tsx
+++ src/application/components/layouts/behaviors/dashboards/client/menu/ClientMenu.tsx
@@ -7,15 +7,22 @@ import { DesktopDashboardMenuLogo } from "@/application/components/layouts/behav
 import { blueLayoutMobileMenuVisibility } from "./blue-layout.mobile-menu"
 import { useStore } from "effector-react"
 import { DashboardMenuContainer } from "@/application/components/layouts/behaviors/dashboards/common/menu/MenuContainer"
+import { $userData } from "@/application/feature/user/user.model"
 
 export const ClientMenu = () => {
   const mobileMenuVisibility = useStore(blueLayoutMobileMenuVisibility)
+  const user = useStore($userData)
 
   return (
     <DashboardMenuContainer showOnMobile={mobileMenuVisibility}>
       <DesktopDashboardMenuLogo />
       <DashboardMenuMobileHeader />
-      <ProfileHeader />
+      <ProfileHeader
+        firstName={user.client?.firstName}
+        lastName={user.client?.lastName}
+        showCoachDropdown={!!user.coach}
+        avatar={user.client?.avatar}
+      />
       <ClientMenuItems />
       <CoachLink>Стать коучем</CoachLink>
     </DashboardMenuContainer>
