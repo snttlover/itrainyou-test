6799c7230b424e0e60f9b7cf93fc1053fd8da3ec
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-06-11 03:20:24 +0300

    Fix trailing slash bug

--- next.config.js
+++ next.config.js
@@ -8,12 +8,12 @@ module.exports = enhance(
   withImages({
     optimizeImagesInDev: true,
     env: {
-      BACKEND_URL: process.env.BACKEND_URL || "https://dev.itrainyou.heksray.com"
+      BACKEND_URL: process.env.BACKEND_URL || "https://dev.itrainyou.heksray.com",
     },
     webpack(config, options) {
       config.resolve.alias["@"] = path.join(__dirname, "src")
       config.resolve.alias["@app"] = path.join(__dirname, "src")
       return config
-    }
+    },
   })
 )
