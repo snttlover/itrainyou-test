34efc1cc9164feb48fb9fd3fc3955313683dd689
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-07-01 22:18:48 +0300

    runInScope

---
+++ src/scope.ts
@@ -0,0 +1,20 @@
+import { changeToken, TOKEN_COOKIE_KEY } from "@/lib/network/token"
+import { Effect, Event, root } from "effector-root"
+import { allSettled, fork, hydrate, Scope } from "effector/fork"
+import Cookies from "js-cookie"
+
+type InferEventEffect<T> = T extends Event<infer U> ? U : T extends Effect<infer U, any> ? U : never
+
+let scope: Scope
+
+export const runInScope = <E extends Event<any> | Effect<any, any, any>>(unit: E, params?: InferEventEffect<E>) =>
+  allSettled(unit, { scope, params })
+
+export const restoreState = async () => {
+  hydrate(root, { values: window.INITIAL_STATE })
+  scope = fork(root)
+
+  await runInScope(changeToken, Cookies.get(TOKEN_COOKIE_KEY))
+
+  return scope
+}
