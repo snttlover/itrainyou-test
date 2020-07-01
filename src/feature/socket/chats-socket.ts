8dd7b61eb0ac90ecea0262f13423bfe03ce1efd2
vlkf27 <vlkf27@users.noreply.github.com>
2020-07-01 23:16:45 +0300

    feat: WS_HOST

--- src/feature/socket/chats-socket.ts
+++ src/feature/socket/chats-socket.ts
@@ -18,10 +18,8 @@ export type SocketChatMessage = {
 
 type UserType = "client" | "coach"
 
-const WS_HOST = `ws${(config.BACKEND_URL || ``).replace(/^https/, ``)}`
-
 const getChatSocketLink = (type: UserType, token: string) => {
-  return `ws://142.93.228.206:8006/ws/chat/${type}/?token=${token}`
+  return `${config.WS_HOST}/ws/chat/${type}/?token=${token}`
 }
 
 export const createChatsSocket = (userType: UserType) => {
