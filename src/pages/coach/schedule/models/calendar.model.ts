a3f6100afe7e0e91cb0ba54cb21763504deb4af8
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-07-06 22:13:42 +0300

    Fix calendar month change

--- src/pages/coach/schedule/models/calendar.model.ts
+++ src/pages/coach/schedule/models/calendar.model.ts
@@ -1,9 +1,9 @@
 import { date } from "@/lib/formatting/date"
 import { Dayjs } from "dayjs"
-import { createEvent, createStore } from "effector-root"
+import { createEvent, restore } from "effector-root"
 
 export const setCurrentMonth = createEvent<Dayjs>()
 
-export const $currentMonth = createStore(date())
+export const $currentMonth = restore(setCurrentMonth, date())
 export const $monthStartDate = $currentMonth.map(dat => date(dat).date(1))
 export const $monthEndDate = $monthStartDate.map(dat => date(dat).add(1, "month"))
