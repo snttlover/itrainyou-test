3573630b785b3cdb7940d354d171b41d56260c02
Ivan <ivan.sherstnyuk@gmail.com>
2020-07-10 16:47:57 +0300

    Fix infinite scroll overflow

--- src/pages/client/home/HomePage.tsx
+++ src/pages/client/home/HomePage.tsx
@@ -125,6 +125,7 @@ export const HomePage = () => {
             next={_loadMore as any}
             hasMore={isHasMoreRecommendations}
             dataLength={recommendations.length}
+            style={{overflow: 'hidden'}}
           >
             {recommendations.map(coach => (
               <RecommendationCoachCard key={coach.id} coach={coach} />
