bfa1d4f0b983527ed34dddba707937a93ce1718b
vlkf27 <vlkf27@users.noreply.github.com>
2020-07-12 21:38:56 +0300

    feat: Проверить лоадеры

--- src/pages/common/settings/SettingsPage.tsx
+++ src/pages/common/settings/SettingsPage.tsx
@@ -4,12 +4,16 @@ import { GeneralSettingsForm } from "./content/general-settings-form/GeneralSett
 import { PasswordForm } from "./content/PasswordForm"
 import { LogoutButton } from "@/pages/common/settings/content/LogoutButton"
 import { MediaRange } from "@/lib/responsive/media"
+import { getMyUserFx } from "@/lib/api/users/get-my-user"
+import { useStore } from "effector-react/ssr"
+import { Loader } from "@/components/spinner/Spinner"
 
 const Container = styled.div`
   width: 100%;
   max-width: 736px;
   margin-top: 36px;
   padding: 0 24px 20px;
+  position: relative;
   ${MediaRange.lessThan(`tablet`)`
     margin: 40px auto 0;
   `}
@@ -26,13 +30,31 @@ const Title = styled.div`
   `}
 `
 
-export const SettingsPage = () => (
-  <Container>
-    <Title>Настройки</Title>
-    <GeneralSettingsForm />
-    <PasswordForm />
-    <LogoutButton />
-  </Container>
-)
+const StyledLoaderWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  position: absolute;
+  background: #eee;
+  left: 0;
+  top: 0;
+`
+
+export const SettingsPage = () => {
+  const pending = useStore(getMyUserFx.pending)
+  return (
+    <Container>
+      {pending && (
+        <StyledLoaderWrapper>
+          <Loader />
+        </StyledLoaderWrapper>
+      )}
+      <Title>Настройки</Title>
+      <GeneralSettingsForm />
+      <PasswordForm />
+      <LogoutButton />
+    </Container>
+  )
+}
 
 export default SettingsPage
