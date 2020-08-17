3d917aada848183cf80aea7b14121bdfee0c07e3
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-08-18 01:31:29 +0300

    [Feat] Timezones

--- src/lib/formatting/date.ts
+++ src/lib/formatting/date.ts
@@ -1,8 +1,12 @@
+import { $userData, UserData } from "@/feature/user/user.model"
+import { getStoreFromScope } from "@/scope"
 import dayjs, { Dayjs } from "dayjs"
 import "dayjs/locale/ru"
 import utc from "dayjs/plugin/utc"
 import isBetween from "dayjs/plugin/isBetween"
 import weekday from "dayjs/plugin/weekday"
+// @ts-ignore
+import tzOffset from "get-timezone-offset"
 
 dayjs.extend(weekday)
 dayjs.extend(isBetween)
@@ -10,7 +14,19 @@ dayjs.extend(utc)
 dayjs.locale("ru")
 
 export const date = (date?: dayjs.ConfigType, option?: dayjs.OptionType, locale?: string): Dayjs => {
-  return dayjs(date, option, locale)
+  let options: Exclude<dayjs.OptionType, string> = {}
+
+  if (typeof option === "string") options.format = option
+  else options = { ...option, utc: true }
+
+  const userData =
+    process.env.BUILD_TARGET === "client" ? getStoreFromScope($userData) : { client: { user: { timeZone: 0 } } }
+
+  const now = new Date()
+
+  const timeZone = -tzOffset(userData?.client?.user?.timeZone, now) //|| -now.getTimezoneOffset()
+
+  return dayjs(date, options, locale).add(timeZone, "minute")
 }
 date.utc = dayjs.utc
 
