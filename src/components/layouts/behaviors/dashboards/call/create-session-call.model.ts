b20221929a18a4afbd62f27519f394a283a8230c
vlkf27 <vlkf27@users.noreply.github.com>
2020-09-07 16:38:55 +0300

    fix: call zone

--- src/components/layouts/behaviors/dashboards/call/create-session-call.model.ts
+++ src/components/layouts/behaviors/dashboards/call/create-session-call.model.ts
@@ -87,6 +87,7 @@ export const createSessionCallModule = (config: CreateSessionCallModuleConfig) =
         agoraData.client = agoraLib.createClient({
           mode: `live`,
           codec: `h264`,
+          areaCode: agoraLib.AREAS.EUROPE
         }) as AgoraClient
         const appId = appConfig.AGORA_ID as string
         agoraData.client.init(appId, () => {}, agoraHandleFail)
