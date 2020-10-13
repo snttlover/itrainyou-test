78d638fa406888cdd99ea6d981a60e24dda9db7f
vlkf27 <vlkf27@users.noreply.github.com>
2020-10-14 02:29:05 +0300

    fix: Если недостаточно денег, должна появляться ошибка "недостаточно средств", а появляется "не удалось вывести деньги".

--- src/pages/coach/wallet/withdraw-dialog/withdraw.model.ts
+++ src/pages/coach/wallet/withdraw-dialog/withdraw.model.ts
@@ -71,9 +71,12 @@ const startWithdrawFx = createEffect({
 
 export const $isTopUpLoading = startWithdrawFx.pending
 
-const failMessage: Toast = { text: "Не удалось вывести деньги", type: "error" }
+const failMessage: Toast = { text: '', type: "error" }
 forward({
-  from: startWithdrawFx.failData.map(_ => failMessage),
+  from: startWithdrawFx.failData.map(error => {
+    failMessage.text = (error as any)?.response.data.non_field_errors.toString() || "Не удалось вывести деньги"
+    return failMessage
+  }),
   to: [toasts.remove, toasts.add],
 })
 
