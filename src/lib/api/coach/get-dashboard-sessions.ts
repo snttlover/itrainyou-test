33cd876b61aadc401c56a615234d42309aa1f2f1
cherrypaff <osdoka@mail.ru>
2021-01-16 16:15:32 +0300

    Пофиксил количество пройденных сессий

--- src/lib/api/coach/get-dashboard-sessions.ts
+++ src/lib/api/coach/get-dashboard-sessions.ts
@@ -19,6 +19,7 @@ export interface DashboardSession {
   recordingUrl: string
   hasAwaitingRescheduleRequests: boolean
   materials: []
+  mutualSessionsCount: number
 }
 
 type getDashboardSessionsParams = {
