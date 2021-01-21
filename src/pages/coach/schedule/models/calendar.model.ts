2a570384e7bba25fc2a6f25cfd4a10aea23be4e5
cherrypaff <osdoka@mail.ru>
2021-01-21 18:15:40 +0300

    Фикс текущей даты месяца

--- src/pages/coach/schedule/models/calendar.model.ts
+++ src/pages/coach/schedule/models/calendar.model.ts
@@ -5,6 +5,14 @@ import { createEvent, restore } from "effector-root"
 export const setCurrentMonth = createEvent<Dayjs>()
 export const changeDate = createEvent<Dayjs>()
 
-export const $currentMonth = restore(setCurrentMonth, date()).on(changeDate, (_, payload) => payload)
+/*export const $currentMonth = restore(setCurrentMonth, date()).on(changeDate, (_, payload) => payload)
+export const $monthStartDate = $currentMonth.map(dat => date(dat).date(1))
+export const $monthEndDate = $monthStartDate.map(dat => date(dat).add(1, "month"))*/
+
+export const $currentMonth = restore(setCurrentMonth, date())
 export const $monthStartDate = $currentMonth.map(dat => date(dat).date(1))
 export const $monthEndDate = $monthStartDate.map(dat => date(dat).add(1, "month"))
+
+$currentMonth.watch(month => console.log("currentMonth", month))
+$monthStartDate.watch(month => console.log("MonthstartDate", month))
+$monthEndDate.watch(month => console.log("MonthEndDate", month))
