55dc8e3429d2056e131891c77c1aa36ed37d6194
mebelh <memetbelualov@bk.ru>
2021-02-03 15:23:31 +0300

    feat: На 3 этапе регистрации ошибку в датах выводим только когда все заполнено

--- src/feature/chat/view/content/message-box/content/MessageBoxUpload.tsx
+++ src/feature/chat/view/content/message-box/content/MessageBoxUpload.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react"
+import React, { useCallback, useEffect, useRef } from "react"
 import styled from "styled-components"
 import { Icon } from "@/components/icon/Icon"
 import { FileRejection, useDropzone } from "react-dropzone"
