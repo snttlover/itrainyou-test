88b4046982c798512ff72ac8e479a0f4530ec65f
jason parser <cucya100@gmail.com>
2021-05-07 16:57:19 +0300

    coach landing hero

---
+++ src/pages/landing-coach/common/RegisterButton.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
+export const RegisterButton = styled(Link)`
+  padding: 9px 24px;
+  background: #ff6b00;
+  border: none;
+  outline: none;
+  border-radius: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 22px;
+  text-align: center;
+  color: #ffffff;
+`
--- src/pages/landing-coach/content/Header.tsx
+++ src/pages/landing-coach/content/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 
+import { Link } from "react-router-dom"
+import { routeNames } from "@/pages/route-names"
+
 import { Container } from "../common/Container"
+import { RegisterButton } from "../common/RegisterButton"
+
+import logoImg from "../assets/header/logo.svg"
 
 const Wrapper = styled.header`
   width: 100%;
@@ -15,10 +21,62 @@ const StyledContainer = styled(Container)`
   color: white;
 `
 
+const LogoLink = styled(Link)`
+  cursor: pointer;
+  display: block;
+  flex-shrink: 0;
+`
+
+const Logo = styled.img`
+  height: 36px;
+  display: block;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`
+
+const NavLink = styled(Link)`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 22px;
+  text-align: center;
+  color: #ffffff;
+  cursor: pointer;
+
+  &:not(:first-child) {
+    margin-left: 40px;
+  }
+
+  &:hover {
+    color: #e1e6ea;
+  }
+
+  &:active {
+    text-decoration-line: underline;
+    color: #e1e6ea;
+  }
+`
+
+const StyledRegisterButton = styled(RegisterButton)`
+  margin-left: 32px;
+`
+
 export const Header = () => (
   <Wrapper>
     <StyledContainer>
-      <p>Header</p>
+      <LogoLink to={{ pathname: routeNames.landing(), search: "" }}>
+        <Logo src={logoImg} />
+      </LogoLink>
+      <Nav>
+        <NavLink to={{ pathname: routeNames.search() }}>Найти коуча</NavLink>
+        <NavLink to={routeNames.login()}>Войти</NavLink>
+      </Nav>
+      <StyledRegisterButton to={routeNames.signup("1")}>Зарегистрироваться</StyledRegisterButton>
     </StyledContainer>
   </Wrapper>
 )
