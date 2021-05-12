c47994224bb7465134bf50fe58340cf01ecd82e8
jason parser <cucya100@gmail.com>
2021-05-12 21:50:16 +0300

    review fixes

--- src/pages/landing-coach/content/Publications.tsx
+++ src/pages/landing-coach/content/Publications.tsx
@@ -1,34 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Mobile } from "./publications/Mobile"
-import { Desktop } from "./publications/Desktop"
-
 const Wrapper = styled.section`
   background: #4858cc;
 
   margin-bottom: 84px;
 `
 
-const MobileVersion = styled(Mobile)`
-  display: block;
-
-  @media (min-width: 1140px) {
-    display: none;
-  }
-`
-
-const DesktopVersion = styled(Desktop)`
-  display: none;
-
-  @media (min-width: 1140px) {
-    display: block;
-  }
-`
-
 export const Publications = () => (
   <Wrapper>
-    <MobileVersion />
-    <DesktopVersion />
+    <p>Publications Slider</p>
   </Wrapper>
 )
--- src/pages/landing-coach/content/features/Desktop.tsx
+++ src/pages/landing-coach/content/features/Desktop.tsx
@@ -4,8 +4,7 @@ import styled from "styled-components"
 import { content } from "./content"
 
 import { Container } from "../../common/Container"
-
-import arrowIcon from "../../assets/features/desktop/arrow-down.svg"
+import { NavItem } from "./desktop/NavItem"
 
 const StyledContainer = styled(Container)`
   height: 100%;
@@ -34,79 +33,19 @@ const Nav = styled.nav`
   width: 380px;
 `
 
-const NavItem = styled.div`
-  position: relative;
-  padding-left: 20px;
-  cursor: pointer;
-
-  &:not(:last-child) {
-    margin-bottom: 33px;
-  }
-
-  h3 {
-    width: 278px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 24px;
-    line-height: 32px;
-    color: #ffffff;
-    margin-bottom: ${props => (props.active ? "16px" : "0px")};
-  }
-
-  p {
-    width: 328px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
-    line-height: 22px;
-    color: #ffffff;
-  }
-`
-
-const Progress = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 4px;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.6);
-  border-radius: 8px;
-
-  &::after {
-    position: absolute;
-    content: "";
-    width: 4px;
-    height: ${props => (props.progress ? props.progress : "0")}%;
-    left: 0px;
-    top: 0px;
-    background: #ffffff;
-    border-radius: 8px;
-  }
-`
-
-const Arrow = styled.i`
-  display: block;
-  position: absolute;
-  top: 8px;
-  right: 5px;
-  width: 14px;
-  height: 8px;
-  background: url("${arrowIcon}");
-  transform: rotate(${props => (props.reverse ? "0deg" : "180deg")});
-`
-
-const CurrentImage = styled.img`
+const CurrentImage = styled.img<{ topOffset: number }>`
   position: absolute;
   top: ${props => (props.topOffset ? props.topOffset : "40")}px;
   right: 0;
   max-width: 600px;
 `
 
-export const Desktop = ({ className }) => {
+type Props = {
+  className: any
+}
+
+export const Desktop = ({ className }: Props) => {
   const [activeFeature, setActiveFeature] = useState(0)
-  const [progress, setProgress] = useState(0)
   const [userReachedBlock, setUserReachedBlock] = useState(false)
 
   const isBottom = (el: any) => {
@@ -119,8 +58,6 @@ export const Desktop = ({ className }) => {
     if (isBottom(wrappedElement)) {
       if (userReachedBlock) return
 
-      setActiveFeature(0)
-      setProgress(0)
       setUserReachedBlock(true)
     }
   }
@@ -128,20 +65,18 @@ export const Desktop = ({ className }) => {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll)
 
-    const timer = setInterval(() => {
-      if (progress === 100) {
-        setProgress(0)
-        setActiveFeature(activeFeature === content.length - 1 ? 0 : activeFeature + 1)
-      } else {
-        setProgress(progress + 0.25)
-      }
-    }, 25)
-
     return function cleanup() {
       window.removeEventListener("scroll", handleScroll)
-      clearInterval(timer)
     }
   })
+
+  const goToNext = () => {
+    if (activeFeature === content.length - 1) {
+      setActiveFeature(0)
+    } else {
+      setActiveFeature(activeFeature + 1)
+    }
+  }
 
   return (
     <StyledContainer className={className} id='features-desktop-block'>
@@ -150,19 +85,15 @@ export const Desktop = ({ className }) => {
         {content.map((item, index) => (
           <NavItem
             key={item.id}
-            onClick={() => {
-              setActiveFeature(index)
-              setProgress(0)
-            }}
-            active={activeFeature === index}
-          >
-            {activeFeature === index ? <Progress progress={progress} /> : ""}
-            <Arrow reverse={activeFeature !== index} />
-            <h3>
-              {item.id}. {item.title}
-            </h3>
-            {activeFeature === index ? <p>{item.descr}</p> : ""}
-          </NavItem>
+            index={index}
+            id={item.id}
+            title={item.title}
+            descr={item.descr}
+            isActive={activeFeature === index}
+            userReachedBlock={userReachedBlock}
+            goToNext={goToNext}
+            setActive={setActiveFeature}
+          />
         ))}
       </Nav>
       <CurrentImage topOffset={content[activeFeature].desktopTopOffset} src={content[activeFeature].image} />
