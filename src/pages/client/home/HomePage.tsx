88ba7357339a961751d8eb8d598481fc352d0630
osdoka <osdoka@mail.ru>
2021-06-02 13:47:21 +0300

    fix: ts ошибка

--- src/pages/client/home/HomePage.tsx
+++ src/pages/client/home/HomePage.tsx
@@ -2,7 +2,7 @@ import { ClientDashboardLayout } from "@/oldcomponents/layouts/behaviors/dashboa
 import React, { useEffect } from "react"
 import { FreeSessionsHomePage } from "./FreeSessionsHomePage"
 import { StandardHomePage } from "./StandardHomePage"
-import { $hasFreeSessions, getUserData } from "@/pages/client/home/home.model"
+import { $hasFreeSessions } from "@/pages/client/home/home.model"
 import { useStore, useEvent } from "effector-react"
 
 
