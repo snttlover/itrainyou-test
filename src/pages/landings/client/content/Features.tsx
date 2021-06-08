0c8fb34fdfc1d57a129f9bafb06193984db30df6
jason parser <cucya100@gmail.com>
2021-06-08 18:29:36 +0300

    new client land - mobile features

--- src/pages/landings/client/content/Features.tsx
+++ src/pages/landings/client/content/Features.tsx
@@ -1,14 +1,43 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Container } from "../../common/Container"
+import { Mobile } from "./features/Mobile"
+import { Desktop } from "./features/Desktop"
 
-const Wrapper = styled.section``
+const Wrapper = styled.section`
+  background: #4858cc;
+  overflow: hidden;
 
-const StyledContainer = styled(Container)``
+  margin-bottom: 40px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 80px;
+  }
+
+  @media (min-width: 1140px) {
+    margin-bottom: 120px;
+  }
+`
+
+const MobileVersion = styled(Mobile)`
+  display: block;
+
+  @media (min-width: 1140px) {
+    display: none;
+  }
+`
+
+const DesktopVersion = styled(Desktop)`
+  display: none;
+
+  @media (min-width: 1140px) {
+    display: block;
+  }
+`
 
 export const Features = () => (
   <Wrapper>
-    <StyledContainer>Features</StyledContainer>
+    <MobileVersion />
+    <DesktopVersion />
   </Wrapper>
 )
---
+++ src/pages/landings/client/content/features/content.ts
@@ -0,0 +1,35 @@
+import feat1Img from "../../assets/features/1.png"
+import feat2Img from "../../assets/features/2.png"
+import feat3Img from "../../assets/features/3.png"
+import feat4Img from "../../assets/features/4.png"
+
+export const content = [
+  {
+    id: 1,
+    title: "Регистрируйтесь",
+    descr: "Это займет пару минут. Можно войти с помощью социальных сетей!",
+    image: feat1Img,
+    desktopTopOffset: 40,
+  },
+  {
+    id: 2,
+    title: "Выбирайте коуча",
+    descr: "Мы сделаем подборку коучей под ваш запрос. Все коучи платформы сертифицированы и проверены.",
+    image: feat2Img,
+    desktopTopOffset: 60,
+  },
+  {
+    id: 3,
+    title: "Бронируйте сессию",
+    descr: "Выбирайте удобные дату и время и ждите оповещения. Можете пообщаться с коучем в чате.",
+    image: feat3Img,
+    desktopTopOffset: 84,
+  },
+  {
+    id: 4,
+    title: "Начинайте сессию",
+    descr: "Работайте над собой из любой точки мира в удобное для вас время.",
+    image: feat4Img,
+    desktopTopOffset: 84,
+  },
+]
