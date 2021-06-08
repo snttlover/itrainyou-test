f94e5384bf53d5e0c9691265ea1fa7cf2e481e44
jason parser <cucya100@gmail.com>
2021-06-08 12:50:40 +0300

    new client land hero

--- src/pages/landings/client/content/Hero.tsx
+++ src/pages/landings/client/content/Hero.tsx
@@ -15,49 +15,60 @@ const Wrapper = styled.section`
   background: url("${bgImgSmall}");
   background-position: bottom center;
   background-size: cover;
-  margin-bottom: 40px;
+  margin-bottom: 123px;
 
   @media (min-width: 768px) {
     background: url("${bgImgMed}");
     background-position: bottom center;
     background-size: cover;
+    margin-bottom: 40px;
   }
 
   @media (min-width: 1140px) {
     background: url("${bgImg}");
     background-position: bottom center;
     background-size: cover;
+    margin-bottom: 76px;
   }
 `
 
 const StyledContainer = styled(Container)`
   padding-top: 164px;
-  padding-bottom: 270px;
+  padding-bottom: 181px;
   position: relative;
+
+  @media (min-width: 558px) {
+    padding-top: 164px;
+    padding-bottom: 237px;
+  }
 
   @media (min-width: 768px) {
     padding-top: 116px;
-    padding-bottom: 136px;
+    padding-bottom: 134px;
   }
 
   @media (min-width: 1140px) {
-    padding-top: 143px;
-    padding-bottom: 174px;
+    padding-top: 130px;
+    padding-bottom: 226px;
   }
 `
 
 const Title = styled.h1`
   font-family: Roboto Slab;
   font-style: normal;
-  font-weight: normal;
+  font-weight: bold;
   font-size: 24px;
   line-height: 32px;
   text-align: center;
   color: #ffffff;
   margin: 0 auto;
 
-  max-width: 288px;
+  max-width: 278px;
   margin-bottom: 16px;
+
+  @media (min-width: 558px) {
+    max-width: 100%;
+  }
 
   @media (min-width: 768px) {
     margin: 0;
@@ -65,19 +76,11 @@ const Title = styled.h1`
     line-height: 44px;
     text-align: left;
 
-    max-width: 412px;
+    max-width: 402px;
     margin-bottom: 8px;
   }
 
-  @media (min-width: 1024px) {
-    max-width: 534px;
-  }
-
   @media (min-width: 1140px) {
-    font-size: 40px;
-    line-height: 52px;
-
-    max-width: 534px;
     margin-bottom: 24px;
   }
 `
@@ -92,12 +95,16 @@ const Descr = styled.p`
   color: #efefef;
   margin: 0 auto;
 
-  max-width: 100%;
+  max-width: 288px;
   margin-bottom: 24px;
 
+  @media (min-width: 558px) {
+    max-width: 100%;
+  }
+
   @media (min-width: 768px) {
-    font-size: 20px;
-    line-height: 28px;
+    font-size: 24px;
+    line-height: 32px;
     text-align: left;
     margin: 0;
     margin-bottom: 24px;
@@ -105,15 +112,10 @@ const Descr = styled.p`
     max-width: 412px;
   }
 
-  @media (min-width: 1024px) {
-    margin-bottom: 16px;
-  }
-
   @media (min-width: 1140px) {
     max-width: 553px;
-    font-size: 24px;
-    line-height: 32px;
-    margin-bottom: 32px;
+
+    margin-bottom: 44px;
   }
 `
 
@@ -141,30 +143,26 @@ const StyledRegisterButton = styled(RegisterButton)`
 const MainImage = styled.img`
   display: block;
   position: absolute;
-  bottom: -20px;
+  bottom: -54px;
   right: 50%;
   transform: translateX(50%);
   width: 278px;
 
   @media (min-width: 558px) {
-    bottom: 0;
+    bottom: -69px;
   }
 
   @media (min-width: 768px) {
-    right: 0;
+    right: 10px;
     transform: translateX(0);
-    bottom: 20px;
-    width: 368px;
-  }
-
-  @media (min-width: 1024px) {
-    bottom: 44px;
+    bottom: 70px;
+    width: 377px;
   }
 
   @media (min-width: 1140px) {
-    right: -2px;
-    bottom: -30px;
-    width: 672px;
+    right: 41px;
+    bottom: 33px;
+    width: 508px;
   }
 `
 
