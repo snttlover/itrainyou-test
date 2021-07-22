ca8e598c7e7cf6bbf85286c2750cc5aa8d128254
sepezho <sepezho@gmail.com>
2021-07-22 17:32:24 +0300

    Feat: created tooltip on hover in copy profile link

--- src/pages/coach/profile/profile-page/components/BaseCoachInfo.tsx
+++ src/pages/coach/profile/profile-page/components/BaseCoachInfo.tsx
@@ -147,38 +147,52 @@ const MobileEditButton = styled(EditButton)`
   `}
 `
 
-
-
 const Tooltip = styled.span`
-  visibility: visible;
-  
   background: #FFFFFF;
   border-radius: 8px;
   padding: 12px;
-
+  width: 224px; 
   font-family: Roboto;
   font-style: normal;
   font-weight: normal;
   font-size: 14px;
   line-height: 22px;
   box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.08), 0px 0px 4px rgba(0, 0, 0, 0.16), 0px 6px 18px -6px rgba(0, 0, 0, 0.04);
-
 `
 
 const TooltipContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  position: absolute;
+  transform: translate(-80%, -100%);
+  align-items: flex-end;
+  visibility: hidden;
+  margin: -5px 0 0 10px;
+  > svg {
+    margin: 0 35px 0 -35px;
+  }
+  ${MediaRange.greaterThan("desktop")`
+    transform: translate(-50%, -100%);
+    align-items: center;
+    svg {
+      margin: 0;
+    }
+  `}
 `
 
 const CopyLink = styled(CopyLinkIcon)`
   width: 24px;
   cursor: pointer;
+  position: relative;
+  z-index: 1000;
   stroke: #9aa0a6;
 `
 
 const CopyLinkContainer = styled.div`
   align-self: flex-end;
   margin-left: 5px;
-  ${CopyLink}:hover ${Tooltip} {
+  z-index: 1;
+  &:hover ${TooltipContainer} {
     visibility: visible;
   }
 `
@@ -201,9 +215,9 @@ export const BaseCoachInfo = styled(({ ...props }) => {
                 <Tooltip>
                   Скопировать ссылку профиля
                 </Tooltip>
-                  <svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9.23178 11.0781L0 0H20L10.7682 11.0781C10.3684 11.5579 9.63157 11.5579 9.23178 11.0781Z" fill="white"/>
-                  </svg>
+                <svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9.23178 11.0781L0 0H20L10.7682 11.0781C10.3684 11.5579 9.63157 11.5579 9.23178 11.0781Z" fill="white"/>
+                </svg>
               </TooltipContainer>
 
               <CopyLink link={() => `https://${window.location.hostname}/search/coach/${coach?.id}`} />
