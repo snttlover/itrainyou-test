3f639f5bbefe264e09b8b9e6599ff789ffbb77c2
Spoki4 <ivan.sherstnyuk@gmail.com>
2021-08-02 21:07:13 +0300

    Chore: Выключил @typescript-eslint/explicit-module-boundary-types правило линтера
    Это правило заставляет описывать типы на входе и выходе. Так как мы используем архитектуру на модулях, мы описываем очень много фабрик без указания возвращаемого типа и это правило создает "шум".

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,43 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    "quotes": [
+      "error",
+      "double"
     ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-multiple-empty-lines": [
-            "error",
-            {"max": 2, "maxEOF": 1}
-        ]
-    }
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-multiple-empty-lines": [
+      "error",
+      {"max": 2, "maxEOF": 1}
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  }
 }
