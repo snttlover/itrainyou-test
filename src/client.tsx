b49b73cad91f3bb05cb25d5545a10e11f9cd638f
Spoki4 <ivan.sherstnyuk@gmail.com>
2021-08-14 15:16:43 +0300

    Fix: Удаление повторяющихся кук token

--- src/client.tsx
+++ src/client.tsx
@@ -18,7 +18,7 @@ import * as Sentry from "@sentry/react"
 import { config } from "@/config"
 import { Integrations } from "@sentry/tracing"
 import { enableDebugger } from "@/lib/effector/debug"
-import { sessionToken } from "@/feature/user/session-token"
+import { sessionToken, TOKEN_COOKIE_KEY } from "@/feature/user/session-token"
 
 if (config.ENVIRONMENT !== "local") {
   Sentry.init({
@@ -38,6 +38,21 @@ if (config.ENVIRONMENT !== "local") {
 
 if (config.ENVIRONMENT === "local" && config.DEBUG === "1") {
   enableDebugger()
+}
+
+const cookiesKeys = document.cookie.split("; ").map((cookeStr) => cookeStr.split("=")[0])
+const tokenCookiesCount = cookiesKeys.reduce((acc, key) => acc + Number(key === TOKEN_COOKIE_KEY), 0)
+
+if (tokenCookiesCount > 1) {
+  const currentPathname = document.location.pathname
+  const paths = currentPathname.split("/").filter(path => path.length > 0)
+
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths.slice(0, i + 1).join("/")
+
+    Cookies.remove(TOKEN_COOKIE_KEY, { path: `/${path}` })
+    Cookies.remove(TOKEN_COOKIE_KEY, { path: `/${path}/` })
+  }
 }
 
 const token = sessionToken.get()
