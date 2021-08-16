1b2b5c79afd912da8a3cd56f412e955d0bd4c382
sepezho <sepezho@gmail.com>
2021-08-16 15:17:06 +0300

    Re-rited calendar adapt switcher

--- src/lib/responsive/mediaHooks.ts
+++ src/lib/responsive/mediaHooks.ts
@@ -2,14 +2,14 @@ import { useState, useEffect } from "react"
 
 const ranges = {
   mobile: "480px",
-  tablet: "800px",
+  tablet: "768px",
   laptop: "1024px",
   desktop: "1280px",
 }
 
-export const mediaQuery = (query) => {
+export const mediaQuery = (query: string) => {
   const media = window.matchMedia(query)
-  const [matches, setMatches] = useState()
+  const [matches, setMatches] = useState(true)
   const handler = () => setMatches(media.matches)
 
   useEffect(() => {
@@ -25,3 +25,13 @@ export const isMobile = () => mediaQuery(`(max-width: ${ranges.tablet})`)
 export const isTablet = () => mediaQuery(`(min-width: ${ranges.tablet}) and (max-width: ${ranges.laptop})`)
 export const isLaptop = () => mediaQuery(`(min-width: ${ranges.laptop}) and (max-width: ${ranges.desktop})`)
 export const isDesktop = () => mediaQuery(`(min-width: ${ranges.desktop})`)
+
+export const biggerThenMobile = () => mediaQuery(`(min-width: ${ranges.mobile})`)
+export const biggerThenTablet = () => mediaQuery(`(min-width: ${ranges.tablet})`)
+export const biggerThenLaptop = () => mediaQuery(`(min-width: ${ranges.laptop})`)
+export const biggerThenDesktop = () => mediaQuery(`(min-width: ${ranges.desktop})`)
+
+// export const smallerThenMobile = () => mediaQuery(`(max-width: ${ranges.mobile})`)
+// export const smallerThenTablet = () => mediaQuery(`(max-width: ${ranges.tablet})`)
+// export const smallerThenLaptop = () => mediaQuery(`(max-width: ${ranges.laptop})`)
+// export const smallerThenDesktop = () => mediaQuery(`(max-width: ${ranges.desktop})`)
