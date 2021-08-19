eab79a88b992d49f5741623d0359e0e7d52b8ffd
vlkf27 <vlkf27@users.noreply.github.com>
2021-08-20 02:20:16 +0300

    fix: При отправке запроса на бронь сессии (а также в любых других случаях, связанных с системными сообщениями) в чате системных сообщений как у коуча, так и у клиента нет бейджа, уведомляющего о кол-ве системных сообщений

--- src/feature/chats-list/view/components/list/system/SystemChatLink.tsx
+++ src/feature/chats-list/view/components/list/system/SystemChatLink.tsx
@@ -10,20 +10,24 @@ type SystemChatLinkProps = {
 }
 
 export const SystemChatLink = ({ link, newMessagesCount, systemMessage }: SystemChatLinkProps) => (
-  <Container to={link} data-has-messages={!!newMessagesCount}>
+  <Container to={link}>
     <BellIcon />
     <Info>
       <Header>Уведомления</Header>
       <LastMessage>{systemMessage}</LastMessage>
     </Info>
+    <MessageInfo>
+      <Counter data-hide={!newMessagesCount}>{newMessagesCount}</Counter>
+    </MessageInfo>
   </Container>
 )
 
 const Info = styled.div`
   display: flex;
   flex-direction: column;
-  padding-right: 16px;
   overflow: hidden;
+  flex: 1;
+  width: 50%;
 `
 
 const LastMessage = styled.div`
@@ -44,9 +48,7 @@ const Container = styled(Link)`
   padding: 16px 12px;
   border-radius: 2px;
   align-items: center;
-  &[data-has-messages="true"] {
-    background: ${props => props.theme.colors.primaryBackground};
-  }
+  padding-left: 8px;
 `
 
 const BellIcon = styled(Icon).attrs({ name: "circle-bell" })`
@@ -61,4 +63,30 @@ const Header = styled.div`
   font-size: 14px;
   line-height: 22px;
   color: #424242;
+`
+
+const MessageInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  height: 100%;
+`
+
+const Counter = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  color: #ffffff;
+  background: #ff6b00;
+  border-radius: 16px;
+  padding: 3px 4px;
+  min-width: 23px;
+  display: flex;
+  justify-content: center;
+  margin-right: 3px;
+
+  &[data-hide="true"] {
+    visibility: hidden;
+  }
 `
